f72e0fd6ad149b7a248fd2e71ead2a9e32cd2edc
Danlois Tovar <danlois.tovar@gmail.com>
2021-07-25 21:55:07 -0300

    Se realizaron los post en js y se agregaron al html.

--- js/index.js
+++ js/index.js
@@ -1,27 +1,8 @@
-/* Tollie-imagenes: */
-const logoInimacion = document.getElementsByClassName('logotipo');
-const imagenQuienSoy = document.getElementsByClassName('quien-soy-img');
-const imagenContacto = document.getElementsByClassName('contacto');
-const anima = lottie.loadAnimation({
-  container: logoInimacion[0], // the dom element that will contain the animation
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: '/lottie/developer.json', // the path to the animation json
-});
-
-const anima2 = lottie.loadAnimation({
-  container: imagenQuienSoy[0], // the dom element that will contain the animation
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: '/lottie/developer2.json', // the path to the animation json
-});
-
-const anima3 = lottie.loadAnimation({
-  container: imagenContacto[0], // the dom element that will contain the animation
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: '/lottie/pass.json', // the path to the animation json
+/* slider */
+$(document).ready(function () {
+  $('.bxslider').bxSlider({
+    mode: 'fade',
+    captions: true,
+    slideWidth: 1200,
+  });
 });
