82e888b9222d55d4abda7673181a0021019c7d15
Danlois Tovar <danlois.tovar@gmail.com>
2021-07-29 00:42:52 -0300

    Se agrego el modo dark con jquery.

--- js/index.js
+++ js/index.js
@@ -4,9 +4,8 @@ import {} from '../js/lotttie.js';
 import { iniciarSlider } from '../js/slider.js';
 /* post: */
 import { monstarPost } from '../js/post.js';
-/* modo dark */
-import { modoDark } from './modoDark.js';
+/* cambiar a tema oscuro: */
+import {} from './cambiartema.js';
 
 iniciarSlider();
 monstarPost();
-modoDark();
